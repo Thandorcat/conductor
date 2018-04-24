8e6656a128f5f0fa79d5e168df0ae06160a1b8dc
Andrey Doropey <andry.doropey@gmail.com>
2018-04-24 23:27:06 +0300

    Added core class

--- conductor_core.py
+++ conductor_core.py
@@ -1,22 +1,44 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import json
 import importlib
 from properties import PROVIDERS_DIR
 
-app = Flask(__name__)
-#app.config['SERVER_NAME'] = "0.0.0.0:5900"
-@app.route('/', methods=['POST'])
-def result():
-    json_data = request.json
-    notes = json.loads(json_data)
-    print(notes)
-    for space in notes["project"]:
-        space = list(space.values())[0]  # looks like the most elegant way to get value
-        provider = space.pop("provider")
-        provider_module_path = PROVIDERS_DIR + '.' + provider
-        provider_module = importlib.import_module(provider_module_path)  # import provider as module
-        provider_module.run(space)
-    return 'Performed!'
+class Core:
 
-app.debug = True
-app.run(host='0.0.0.0', port=5999)
+
+    def __init__(self, address='0.0.0.0', port=5999):
+        self.address = address
+        self.port = port
+
+    def start(self):
+        app = Flask(__name__)
+
+        @app.route('/', methods=['POST'])
+        def result():
+            json_data = request.json
+            request_data = json.loads(json_data)
+            print(request_data)
+            response = self.handle_request(request_data)
+            return Response(status=response)
+
+        app.debug = True
+        app.run(host=self.address, port=self.port)
+
+    def handle_request(self, request):
+
+        return True
+
+    def play(self, notes):
+        for space in notes["project"]:
+            space = list(space.values())[0]  # looks like the most elegant way to get value
+            provider = space.pop("provider")
+            provider_module_path = PROVIDERS_DIR + '.' + provider
+            provider_module = importlib.import_module(provider_module_path)  # import provider as module
+            provider_module.run(space)
+
+    def stop(self, instanses):
+        pass
+
+if __name__ == '__main__':
+   core = Core()
+   core.start()
